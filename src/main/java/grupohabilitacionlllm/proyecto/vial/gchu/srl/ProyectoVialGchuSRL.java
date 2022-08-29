1984647991451e86ae28170e40ade0be0e7627b8
Lucas Forni <lucas@172.24.2.139>
2022-08-29 11:21:34 -0300

    Primera version

---
+++ src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/ProyectoVialGchuSRL.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package grupohabilitacionlllm.proyecto.vial.gchu.srl;
+
+/**
+ *
+ * @author lucas
+ */
+public class ProyectoVialGchuSRL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
