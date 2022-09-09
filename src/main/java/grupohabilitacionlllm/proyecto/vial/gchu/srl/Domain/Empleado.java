5a2ec563566345feea7dbe94e7ac6ab0ab5828b2
lucas <lucas@172.24.2.117>
2022-09-09 16:32:43 -0300

    Agregando funcionalidades a clase Empleado, actualizacion de Elemento de seguridad

--- src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Domain/Empleado.java
+++ src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Domain/Empleado.java
@@ -12,22 +12,30 @@ import java.time.Period;
  * @author Tincho
  */
 public class Empleado {
+    private int IdEmpleado;
     private String Nombre;
     private String Apellido;
     private int dni;
     private int Telefono;
     private String direccion;
     private LocalDate FechaNac; 
+    private Especializacion Especializacion;
+    private boolean DadoDeBaja;
 
-    public Empleado(String Nombre, String Apellido, int dni, int Telefono, String direccion, LocalDate FechaNac) {
+    public Empleado(int idEmpleado, String Nombre, String Apellido, int dni, int Telefono, String direccion, LocalDate FechaNac) {
+        this.IdEmpleado= idEmpleado;
         this.Nombre = Nombre;
         this.Apellido = Apellido;
         this.dni = dni;
         this.Telefono = Telefono;
         this.direccion = direccion;
         this.FechaNac = FechaNac;
+        this.DadoDeBaja=false;
     }
 
+    public int getidEmpleado(){
+        return this.IdEmpleado;
+    }
     
     
     public String getNombre() {
@@ -84,9 +92,58 @@ public class Empleado {
         this.FechaNac = FechaNac;
     }
     
+    public void setDadoDeBaja(boolean unValor) {
+        this.DadoDeBaja = unValor;
+    }
+
+    public boolean getDadoDeBaja() {
+        return this.DadoDeBaja;
+    }
 
     
+    public boolean verificarEmpleadoDNI(int unDNI) {
+        return (unDNI == this.dni);
+    }
+            
+    public boolean verificarEmpleadoNombreyApellido(String unNombre, String unApellido){
+        return ((unNombre.equals(this.Nombre)) && (unApellido.equals(this.Apellido)));
+       
+    }
     
+    public void setEspecializacion (Especializacion unaEspecializacion){
+       
+        if (this.Especializacion== null) {
+              // block of code to be executed if the condition is true
+             this.Especializacion= unaEspecializacion;
 }
+        else {
+            // block of code to be executed if the condition is false
+            this.finalizarEspecializacion(LocalDate.now());
+            this.Especializacion=unaEspecializacion;
+}
+
+    }
+    
+    
+    public void finalizarEspecializacion(LocalDate unDia){
+        this.Especializacion.fechaFin=unDia;
+    }
+    
+    public void modificarEmpleado(Empleado unEmpleado){
+        this.Apellido= unEmpleado.Apellido;
+        this.Nombre= unEmpleado.Nombre;
+        this.dni= unEmpleado.dni;
+        this.direccion= unEmpleado.direccion;
+        this.FechaNac=unEmpleado.FechaNac;
+        this.Telefono= unEmpleado.Telefono;
+        
+        
+}
+    }
+    
+    public void }
+}
+
+
 
 
