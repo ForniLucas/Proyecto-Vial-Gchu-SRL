f2b6b984ab59e9bea66b007b7b86b3c350804024
MARTIN RODRIGO LOPEZ <lopezmr@o365frcuutneduar.onmicrosoft.com>
2022-09-16 18:36:40 -0300

    Controladora Maquinaria implementado, Ver busqueda

--- src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Controladores/ControladorMaquinaria.java
+++ src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Controladores/ControladorMaquinaria.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package grupohabilitacionlllm.proyecto.vial.gchu.srl.Controladores;
+
+import grupohabilitacionlllm.proyecto.vial.gchu.srl.Domain.Maquinaria;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Tincho
  */
 public class ControladorMaquinaria {
-    
+
+    int id = 1;
+
+    List<Maquinaria> maquinarias = new ArrayList<>();
+
+    public void alta(String cod, String des, String fab, String ubic) {
+
+        Maquinaria maquina = new Maquinaria(id, cod, des, ubic, ubic);
+
+        maquinarias.add(maquina);
+
+        id++;
+    }
+
+    public Maquinaria buscar(String cod) {
+        Maquinaria encontro = null;
+
+        try {
+            for (Maquinaria maq_temp : maquinarias) {
+                if (maq_temp.getCodigo().equals(cod)) {
+                    encontro = maq_temp;
+                }
+            }
+        } catch (Exception e) {
+        }
+
+        return encontro;
+    }
+
+    public void bajaLogica(Maquinaria unaMaquina) {
+        try {
+            for (Maquinaria maq_temp : maquinarias) {
+                if (maq_temp.equals(unaMaquina)) {
+                    maq_temp.setEstadoBaja();
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void baja(Maquinaria unaMaquinaria) {
+        try {
+            maquinarias.remove(unaMaquinaria);
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void baja() {
+
+    }
 }
