f5f7c36a3dd06be9796f1e9182c22faa84eed3c5
lucas <lucas@172.24.2.117>
2022-09-16 16:26:58 -0300

    modificacion clase maquinaria y empleado

--- src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Domain/Empleado.java
+++ src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/Domain/Empleado.java
@@ -12,7 +12,7 @@ import java.time.Period;
  * @author Tincho
  */
 public class Empleado {
-    private int Id;
+    private int id;
     private String Nombre;
     private String Apellido;
     private int dni;
@@ -23,7 +23,7 @@ public class Empleado {
     private boolean DadoDeBaja;
 
     public Empleado(int idEmpleado, String Nombre, String Apellido, int dni, int Telefono, String direccion, LocalDate FechaNac) {
-        this.Id= idEmpleado;
+        this.id= idEmpleado;
         this.Nombre = Nombre;
         this.Apellido = Apellido;
         this.dni = dni;
@@ -34,7 +34,7 @@ public class Empleado {
     }
 
     public int getidEmpleado(){
-        return this.Id;
+        return this.id;
     }
     
     
