9784213f160f15240f0bda12c27e39529e2e2acd
MARTIN RODRIGO LOPEZ <lopezmr@o365frcuutneduar.onmicrosoft.com>
2022-11-13 10:40:37 -0300

    Se agrego SesionesHibernateUtil

---
+++ src/main/java/grupohabilitacionlllm/proyecto/vial/gchu/srl/SesionesHibernateUtil.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupohabilitacionlllm.proyecto.vial.gchu.srl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Tincho
+ */
+public class SesionesHibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new Configuration().configure("/hibernateConfig/hibernate.cfg.xml").buildSessionFactory();
+        } catch (HibernateException ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
